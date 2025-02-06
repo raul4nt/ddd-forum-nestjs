c5b94c28ac53b89819f9f4c09c158b8a4d4de3c0
Raul Silveira <raulantsilveira@gmail.com>
2025-02-06 20:33:34 -0300

    create question getters and setters

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -17,6 +17,41 @@ export class Answer extends Entity<AnswerProps> {
         // props do constructor pai(Entity))
     }
 
+    get authorId() {
+        return this.props.authorId
+    }
+    get questionId() {
+        return this.props.questionId
+    }
+    get createdAt() {
+        return this.props.createdAt
+    }
+    get updatedAt() {
+        return this.props.updatedAt
+    }
+
+    get excerpt() {
+      // excerpt significa trecho(pega um trecho da resposta)
+      return this.content
+      .substring(0, 120)
+      .trimEnd()
+      .concat('...')
+      // cria uma substring do content q vai do 0 ao 120(caracteres), 
+      // depois usa o trim pra tirar os espaços do final e concatena com '...'
+      // pra mostrar que continua(preview da resposta)
+    }
+
+    private touch() {
+      this.props.updatedAt = new Date()
+      // método que chamamos quando vamos setar(atualizar) algo
+    }
+
+    set content(content: string) {
+      this.props.content = content
+      this.touch()
+      // usando o touch pra alterar o updatedAt
+    }
+
         static create(
         // funçao estatica padrao pra toda entidade(criar ela)
           props: Optional<AnswerProps, 'createdAt'>,
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -2,6 +2,8 @@ import { Slug } from "./value-objects/slug"
 import { Entity } from "../../core/entities/entity"
 import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { Optional } from "../../core/types/optional"
+import dayjs from "dayjs"
+
 
 interface QuestionProps {
     authorId: UniqueEntityID 
@@ -14,13 +16,84 @@ interface QuestionProps {
 }
 
 export class Question extends Entity<QuestionProps> {
+
+  get authorId() {
+    return this.props.authorId
+  }
+  get bestAnswerId() {
+      return this.props.bestAnswerId
+  }
+  get title() {
+      return this.props.title
+  }
+  get content() {
+      return this.props.content
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3
+    // se a diferença de dias for menor ou igual a 3, é novo
+  }
+
+  
+  get excerpt() {
+    // excerpt significa trecho(pega um trecho da resposta)
+    return this.content
+    .substring(0, 120)
+    .trimEnd()
+    .concat('...')
+    // cria uma substring do content q vai do 0 ao 120(caracteres), 
+    // depois usa o trim pra tirar os espaços do final e concatena com '...'
+    // pra mostrar que continua(preview da resposta)
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+    // método que chamamos quando vamos setar(atualizar) algo
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+    // usando o touch pra alterar o updatedAt
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+    // quando atualiza o titulo, atualiza o slug tb
+    this.touch()
+    // usando o touch pra alterar o updatedAt
+  }
+  
+
+
     static create(
-      props: Optional<QuestionProps, 'createdAt'>,
+      props: Optional<QuestionProps, 'createdAt' | 'slug'>,
       id?: UniqueEntityID
     ) {
       const question = new Question(
         {
           ...props,
+          slug: props.slug ?? Slug.createFromText(props.title),
+          // torna o slug opcional(se já tiver, usa, se nao, cria baseado no title)
           createdAt: new Date(),
         },
         id
