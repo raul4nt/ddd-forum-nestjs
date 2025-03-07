96805e1fde71485c2254307085e161fc36d62e91
Raul <raulantsilveira@gmail.com>
2025-03-07 14:04:36 -0300

    feat: create fetch answer comments use case

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
@@ -6,7 +7,33 @@ export class InMemoryAnswerCommentsRepository
 {
   public items: AnswerComment[] = []
 
+  async findById(id: string) {
+    const answerComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!answerComment) {
+      return null
+    }
+
+    return answerComment
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answerComments
+  }
+
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment)
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id,
+    )
+
+    this.items.splice(itemIndex, 1)
   }
 }
