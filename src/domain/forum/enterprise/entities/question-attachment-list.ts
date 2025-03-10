ebff3b9e348957f31c6dd2d56ea9be278020a6a1
Raul <raulantsilveira@gmail.com>
2025-03-10 15:57:32 -0300

    refactor delete question use case

--- src/domain/forum/enterprise/entities/question-attachment-list.ts
+++ src/domain/forum/enterprise/entities/question-attachment-list.ts
@@ -3,18 +3,6 @@ import { QuestionAttachment } from './question-attachment'
 
 export class QuestionAttachmentList extends WatchedList<QuestionAttachment> {
   compareItems(a: QuestionAttachment, b: QuestionAttachment): boolean {
-    return a.attachmentId === b.attachmentIdimport { PaginationParams } from '@/core/repositories/pagination-params'
-    import { AnswerComment } from '../../enterprise/entities/answer-comment'
-    
-    export interface AnswerCommentsRepository {
-      findById(id: string): Promise<AnswerComment | null>
-      findManyByAnswerId(
-        answerId: string,
-        params: PaginationParams,
-      ): Promise<AnswerComment[]>
-      create(answerComment: AnswerComment): Promise<void>
-      delete(answerComment: AnswerComment): Promise<void>
-    }
-    
+    return a.attachmentId === b.attachmentId
   }
 }
